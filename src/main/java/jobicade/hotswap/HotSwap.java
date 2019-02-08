9b1a8fa822eefd5aee9188513b84c3699870ee9f
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-02-08 15:26:52 +0000

    Correctly bound Minecraft version range

--- src/main/java/jobicade/hotswap/HotSwap.java
+++ src/main/java/jobicade/hotswap/HotSwap.java
@@ -10,9 +10,17 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
+/*
+ * Dependencies commented out because Forge changed modid at the beginning
+ * of 1.11, from "Forge" to "forge". The minimum version is somewhere in
+ * Forge 1.10, so it is impossible to be compatible with both 1.10 and 1.11
+ * while checking for Forge. There is also no point in checking the
+ * dependency because all version of Forge starting at 1.11 satisfy it.
+ */
+
 @Mod(modid=HotSwap.MODID, name="HotSwap", version="0.1",
-    dependencies="required-after:forge@[12.18.1.2092,)", // minimum Forge/upper bound: EventBusSubscriber
-    acceptedMinecraftVersions="[1.10.2,1.12.2]",
+//    dependencies="required-after:forge@[12.18.1.2092,)", // minimum Forge/upper bound: EventBusSubscriber
+    acceptedMinecraftVersions="[1.11,1.13)",
     acceptableRemoteVersions="*")
 public class HotSwap {
     public static final String MODID = "hotswap";
