edf714a1345e286d54c460245aa5b60a5ab39285
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-02-09 18:49:53 +0000

    Register keys correctly and add category

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Key binding category and improved wording
 ### Fixed
 - Crash on dedicated server when player switches (#2)
 - Inventory Tweaks no longer auto-refills slots emptied by switching
--- src/main/java/jobicade/hotswap/ClientProxy.java
+++ src/main/java/jobicade/hotswap/ClientProxy.java
@@ -8,6 +8,7 @@ import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
@@ -15,16 +16,21 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public final class ClientProxy extends HotSwapProxy {
-    private final KeyBinding CURRENT_UP = new KeyBinding("key.hotswap.rotateUp", Keyboard.KEY_K, "key.categories.misc");
-    private final KeyBinding CURRENT_DOWN = new KeyBinding("key.hotswap.rotateDown", Keyboard.KEY_J, "key.categories.misc");
-    private final KeyBinding ROW_UP = new KeyBinding("key.hotswap.rotateRowUp", Keyboard.KEY_L, "key.categories.misc");
-    private final KeyBinding ROW_DOWN = new KeyBinding("key.hotswap.rotateRowDown", Keyboard.KEY_H, "key.categories.misc");
+    private final KeyBinding CURRENT_UP = new KeyBinding("key.hotswap.rotateUp", Keyboard.KEY_K, "key.categories.hotswap");
+    private final KeyBinding CURRENT_DOWN = new KeyBinding("key.hotswap.rotateDown", Keyboard.KEY_J, "key.categories.hotswap");
+    private final KeyBinding ROW_UP = new KeyBinding("key.hotswap.rotateRowUp", Keyboard.KEY_L, "key.categories.hotswap");
+    private final KeyBinding ROW_DOWN = new KeyBinding("key.hotswap.rotateRowDown", Keyboard.KEY_H, "key.categories.hotswap");
 
     private InvTweaksSuppressor suppressor;
 
     @Override
     public void init() {
         MinecraftForge.EVENT_BUS.register(this);
+
+        ClientRegistry.registerKeyBinding(CURRENT_UP);
+        ClientRegistry.registerKeyBinding(CURRENT_DOWN);
+        ClientRegistry.registerKeyBinding(ROW_UP);
+        ClientRegistry.registerKeyBinding(ROW_DOWN);
 
         if(Loader.isModLoaded("inventorytweaks")) {
             suppressor = new InvTweaksSuppressor();
